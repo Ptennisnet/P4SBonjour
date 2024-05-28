5cb50cd11b0e5d91e5797aca26be1f19997dff99
Ptennisnet <148290329+Ptennisnet@users.noreply.github.com>
2024-05-28 16:00:24 +1000

    BasicUi

--- P4SBonjour/bonjour_app/views.py
+++ P4SBonjour/bonjour_app/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Item
+from .forms import ItemForm
 
-# Create your views here.
+def home(request):
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
+
+
+def add_item(request):
+    if request.method == 'POST':
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = ItemForm()
+    return render(request, 'add_item.html', {'form': form})
+
+
+def view_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    return render(request, 'view_item.html', {'item': item})
