d3e2a5688a8352a0e4ab2ed717b9ee5453ccbad9
Ptennisnet <148290329+Ptennisnet@users.noreply.github.com>
2024-06-13 14:32:38 +1000

    Logging

--- P4SBonjour/bonjour_app/admin.py
+++ P4SBonjour/bonjour_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Supplier, Item, Inventory, Warehouse
 
 # Register your models here.
+
+admin.site.register(Supplier)
+admin.site.register(Item)
+admin.site.register(Inventory)
+admin.site.register(Warehouse)
